13a32dcc27c6829dc69c63f688d871e193942905
John Snow <thelunaticscripter@outlook.com>
2019-03-13 10:36:05 -0400

    Add profile build to webhook
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- bot.rb
+++ bot.rb
@@ -8,7 +8,7 @@ class API < Sinatra::Base
 
     request_data['failed_critical_profiles'].each do |profile|
       profile_path = "./new_profiles/#{profile['name']}_#{profile['sha256']}"
-      new_profile = WaiverProfile.new(profile, profile_path)
+      new_profile = WaiverProfile.new(profile, profile_path, "waiver_#{profile['name']}", '0.1.0', profile['name'], 'thlunaticscripter', './template', profile['controls'])
       new_profile.build_profile
     end
     response
